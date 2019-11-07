69e4133dc96eda78409d671fa2a0f658105d0233
UltimateN00b <leia@gillyweed.co.za>
2019-11-07 14:34:31 +0200

    Can now make a proofing copy from dialogue nodes.

---
+++ Prototype3/Assets/PrintNodes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintNodes : MonoBehaviour
+{
+    public bool printProofCopy;
+    public bool printNodeNumber;
+    public int startFrom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (printProofCopy)
+        {
+            string proofCopy = "";
+
+            for (int i = startFrom; i < this.transform.childCount; i++)
+            {
+                GameObject currChild = this.transform.GetChild(i).gameObject;
+
+                proofCopy += currChild.GetComponent<MainText>().GetCurrCharacter() + ": ";
+                proofCopy += currChild.GetComponent<MainText>().GetMainText() + "*";
+
+                for (int j = 0; j < currChild.transform.childCount; j++)
+                {
+                    if (currChild.transform.GetChild(j).name.Contains("NodeNum"))
+                    {
+                        if (printNodeNumber)
+                        {
+                            proofCopy += "\nNode " + currChild.transform.GetChild(j).GetComponent<Text>().text + ":";
+                        }
+
+                    }
+                    else
+                    {
+                        proofCopy += "\n";
+                        proofCopy += "Choice " + j + ": ";
+                        proofCopy += currChild.transform.GetChild(j).GetComponent<Choice>().GetChoiceText();
+                    }
+                }
+
+                proofCopy += "\n";
+            }
+
+            Debug.Log(proofCopy);
+        }
+
+    }
+}
